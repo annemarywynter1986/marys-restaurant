dc37ee78cb697a47ca4d1230982c84475dc7f8a8
Anne Wynter <missannemary1@gmail.com>
2023-08-08 21:15:32 -0400

    stack and queues

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3001;
 let pagerStack;
 let pagerQueue;
 
+let emptyTableStack;
+let seatedTableQueue;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,32 +26,63 @@ app.post('/addpagers', async (req, res) => {
     pagerStack = new Stack(req.body)
     pagerQueue = new Queue()
 
-    res.status(200).json({ pagerStack, pagerQueue })
+    emptyTableStack = new Stack([1, 2, 3])
+    seatedTableQueue = new Queue()
+
+    res.status(200).json({ pagerStack, pagerQueue, emptyTableStack ,seatedTableQueue })
 })
 
 
-app.post('/customerCheckin', async (req, res) => {
+app.post('/customerDoneEating', async (req, res) => {
+    //customer is done eating left table
 
-    const pager = pagerQueue.removeFromQueue()
-    pagerStack.addToStack(pager)
+    //if theres is a Customer with a Pager than give him the tabel
 
-    res.status(200).json({ pagerStack, pagerQueue })
+    //remove seated table (queue) to an empty table (stack)
+    const freeTable = seatedTableQueue.removeFromQueue()
+    emptyTableStack.addToStack(freeTable)
+
+
+    if (pagerQueue.container.length > 0) {
+        const pager = pagerQueue.removeFromQueue()
+        pagerStack.addToStack(pager)
+
+        const customerTable = emptyTableStack.removeFromStack()
+        seatedTableQueue.addToQueue(customerTable)
+    }
+
+    
+    res.status(200).json({ emptyTableStack , seatedTableQueue })
+       
+
 })
 
 
 app.post('/handToCustomer', async (req, res) => {
-    const pager = pagerStack.removeFromStack()
-    pagerQueue.addToQueue(pager)
 
+    if (emptyTableStack.container.length === 0) {
+        const pager = pagerStack.removeFromStack()
+        pagerQueue.addToQueue(pager)
+        res.status(200).json(pager)
 
-    res.status(200).json(pager)
+    } else {
+        const firstEmptyTable = emptyTableStack.removeFromStack()
+        seatedTableQueue.addToQueue(firstEmptyTable)
+        res.status(200).json(firstEmptyTable)
+    }
 })
 
 app.get('/status', (req, res) => {
 
-    res.status(200).json({ pagerStack, pagerQueue })
+    res.status(200).json({ pagerStack, pagerQueue, emptyTableStack ,seatedTableQueue })
 
 })
 
 
 app.listen(PORT, () => console.log('Now listening http://localhost:3001/'));
+
+
+
+
+
+
